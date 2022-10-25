b2647809662f21523b726afbc73fe022abdc5e57
Corona <Corona09@163.com>
2022-10-25 23:29:34 +0800

    change theme to gruvbox-theme

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -2,9 +2,10 @@
 (set-frame-font "FiraCode Nerd Font 13")
 
 ;; 设置主题
-(pkginstall 'gruvbox-theme)
-(if (display-grayscale-p)
-	(load-theme 'gruvbox-dark-soft t))
+(pkginstall 'dracula-theme)
+(load-theme 'dracula)
+; (if (display-grayscale-p)
+; 	(load-theme 'dracula))
 
 ;; 一个好看的mode-line
 ;; 必须放在主题配置之后
@@ -13,10 +14,15 @@
 	  sml/theme 'respectful)
 (sml/setup)
 
+;; 默认启动打开空窗口
 (setq-default inhibit-startup-screen t)
 (setq inhibit-splash-screen t)
 (setq inhibit-startup-message t)
 (setq initial-scratch-message "")
+
+(setq default-frame-alist '((width . 80)
+                              (height . 30)
+                              (alpha-background . 80)))
 
 ;; 对外提供接口
 (provide 'init-ui)
