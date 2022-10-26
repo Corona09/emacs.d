c84e3971780c525f7cd11abb5f452caa34f83f9a
Corona <Corona09@163.com>
2022-10-27 00:07:08 +0800

    change crux key binds

--- lisp/pkg/crux.el
+++ lisp/pkg/crux.el
@@ -1,7 +1,6 @@
 ;; 常用操作优化
 (pkginstall 'crux)
 (global-set-key (kbd "C-a")   'crux-move-beginning-of-line)
-(global-set-key (kbd "C-c ^") 'crux-top-join-line)
-(global-set-key (kbd "C-x ,") 'crux-find-user-init-file)
-(global-set-key (kbd "C-k")   'crux-smart-kill-line)
-
+(global-set-key (kbd "C-c j") 'crux-top-join-line)
+(global-set-key (kbd "C-c u") 'crux-find-user-init-file)
+(global-set-key (kbd "C-c k") 'crux-smart-kill-line)
